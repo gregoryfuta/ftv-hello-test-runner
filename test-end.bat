52d51cd7c36b51f0a101f3505435f1b31a333e3c
Grzegorz Futa <gregory.futa@sealighs.io>
2022-12-28 08:35:27 +0200

    Parallel tests + otel tests

--- test-end.bat
+++ test-end.bat
@@ -1,4 +1,5 @@
-set SL_LAB_ID=integ_ftv_d580_helloftvapp
+set SL_LAB_ID=integ_otel_d67e_hellootelapp
+rem set SL_LAB_ID=integ_ftv_d580_helloftvapp
 rem set SL_LAB_ID=integ_classic_fe4a_helloclassicapp
 set LOGGING=-Dsl.log.toConsole=true -Dsl.log.level=debug
 set HTTP_DEBUG=-Dsl.httpDebugLog=yes
